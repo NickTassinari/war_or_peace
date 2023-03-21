eb2fc6d5f5f0c89015b52e044dc0c6c28cda5bef
Nick Tassinari <ntass7800@gmail.com>
2023-03-21 13:05:38 -0600

    finish high card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,13 +7,17 @@ class Deck
 
   def rank_of_card_at(index)
     @cards[index].rank  
-    
+    #works
     #return rank of card
   end 
 
-  def high_ranking_cards()
+  def high_ranking_cards
     #return array of cards in deck that have rank of 11 or above
-    #use select do method 
+    #try select do method 
+    
+    high_card = cards.select do |card|
+      card.rank > 10 
+    end 
   end 
 
   def percent_high_ranking()
@@ -22,11 +26,13 @@ class Deck
 
   def remove_card
     #remove top card from deck
+    #works 
     @cards.shift
   end 
 
   def add_card(new_card)
     #add one card to end of deck
+    #works 
     @cards << new_card 
   end 
 end 
