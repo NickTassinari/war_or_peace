0e7ad739223a9e68f9c2347c223a3f0a513dc5bd
Nick Tassinari <ntass7800@gmail.com>
2023-03-21 13:49:33 -0600

    Initial commit

---
+++ spec/deck_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec' 
+require '../lib/deck'
+require '../lib/card'
+
+RSpec.describe Deck do 
+  
+  it 'has cards' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to be(cards)
+  end 
+  
+  it 'has rank of cards' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end 
+ 
+  
+
+end 
