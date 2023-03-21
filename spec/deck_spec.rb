ff5db18a3e47155182eef696822ce5f0fbd15684
Nick Tassinari <ntass7800@gmail.com>
2023-03-21 15:50:21 -0600

    add percentage high ranking

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,6 +4,16 @@ require '../lib/card'
 
 RSpec.describe Deck do 
   
+  it 'exists' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+  
   it 'has cards' do 
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -15,6 +25,7 @@ RSpec.describe Deck do
   end 
   
   it 'has rank of cards' do 
+  
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -23,8 +34,29 @@ RSpec.describe Deck do
 
     expect(deck.rank_of_card_at(0)).to eq(12)
     expect(deck.rank_of_card_at(2)).to eq(14)
+    
   end 
+
+  it 'has high ranking cards' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it 'has percent_high_ranking method' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
  
-  
+ 
 
 end 
